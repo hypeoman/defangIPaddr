07d09f3ddef3c7edfbcf3bb822fb5371f8f4aa63
hyperoman <43721640+hypeoman@users.noreply.github.com>
2021-03-13 15:36:47 +0300

    Add files via upload

---
+++ leetcode_1108.cpp
@@ -0,0 +1,28 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main()
+{
+
+}
+
+class Solution {
+public:
+    string defangIPaddr(string address) {
+        std::string result;
+        std::string f;
+
+        for (int i = 0; i < address.length(); i++) {
+            if (address[i] != '.') {
+                result += address[i];
+            }
+
+            else {
+                f = "[.]";
+                result += f;
+            }
+        }
+        return result;
+    }
+};
